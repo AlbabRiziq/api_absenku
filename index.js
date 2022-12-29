7593f1126fc8f95634385fdccec09b1b33a59d3e
AlbabRiziq <albabriziq@gmail.com>
2022-12-29 12:08:37 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-import express from "express";
+import express, { query } from "express";
+import cors from "cors";
+import { Int32, MongoClient } from "mongodb";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const uri = "mongodb://localhost:27017";
+const dbName = "absenku";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = client.db(dbName);
+// Konek ke mongo db
+
+app.use(cors());
+
+client.connect((err, client) => {
+  if (err) {
+    console.log(err);
+  }
+
+  app.get("/akun", (req, res) => {
+    res.send("COY");
+  });
+  app.post("/absen", (req, res) => {
+    console.log(req);
+  });
+
+  app.get("/api/login", (req, res) => {
+    db.collection("userData")
+      .findOne({ nama: req.query.nama, nisn: Int32(req.query.nisn) })
+      .then((data) => {
+        if (data) {
+          res.send(true);
+        } else {
+          res.send(false);
+        }
+        console.log(req.query);
+      });
+  });
 });
 
 app.listen(PORT, () => {
-  console.log("Server started on port 3000");
+  console.log(`Server berjalan di port ${PORT}`);
 });
